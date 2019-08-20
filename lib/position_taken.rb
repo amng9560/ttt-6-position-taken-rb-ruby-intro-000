d7606d07d2b75e3b586ff4d628ec3960fc331ef2
Amy Nguyen <amy.nguyen730@gmail.com>
2019-08-20 00:27:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,4 @@
 # code your #position_taken? method here!
-ttt_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
   taken = nil
    if (board[index] ==  " " || board[index] == "" || board[index] == nil)
